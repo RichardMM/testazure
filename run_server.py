ec71ebcc10cc2468df02412dabfed8028f51a723
RichardMM <rmachaz@gmail.com>
2018-04-17 21:20:31 +0300

    Change back app from manager

--- run_server.py
+++ run_server.py
@@ -3,38 +3,36 @@ Module to start and run the app
 '''
 import logging
 import traceback
-try:
-    from os import environ
-    from myapp import app, manager
-    app=manager
-    if __name__ == '__main__':
-        # db.create_all()
-        
-        HOST = environ.get('SERVER_HOST', 'localhost')
-        try:
-            PORT = int(environ.get('SERVER_PORT', 'localhost'))
-        except ValueError:
-            PORT = 8080
-        app.jinja_env.auto_reload = True
-        app.run(HOST, PORT)
-except (ModuleNotFoundError, ImportError, ImportWarning) as ex:
-    # logging.basicConfig(filename="myapp.log", level=logging.WARNING)
-    # logging.exception(ex)
-    # log import error in new app
-    from flask import Flask
-    app = Flask(__name__)
-    @app.route('/', methods=['GET'])
-    def errohandler():
-        var = traceback.format_exc()
-        return var
-    if __name__ == '__main__':
-        # db.create_all()
-        HOST = environ.get('SERVER_HOST', 'localhost')
-        try:
-            PORT = int(environ.get('SERVER_PORT', 'localhost'))
-        except ValueError:
-            PORT = 8080
-        app.jinja_env.auto_reload = True
-        app.run(HOST, PORT)
+# try:
+from os import environ
+from myapp import app, manager
+if __name__ == '__main__':
+    # db.create_all()
+    
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', 'localhost'))
+    except ValueError:
+        PORT = 8080
+    app.run(HOST, PORT)
+# except (ModuleNotFoundError, ImportError, ImportWarning) as ex:
+#     # logging.basicConfig(filename="myapp.log", level=logging.WARNING)
+#     # logging.exception(ex)
+#     # log import error in new app
+#     from flask import Flask
+#     app = Flask(__name__)
+#     @app.route('/', methods=['GET'])
+#     def errohandler():
+#         var = traceback.format_exc()
+#         return var
+#     if __name__ == '__main__':
+#         # db.create_all()
+#         HOST = environ.get('SERVER_HOST', 'localhost')
+#         try:
+#             PORT = int(environ.get('SERVER_PORT', 'localhost'))
+#         except ValueError:
+#             PORT = 8080
+#         #app.jinja_env.auto_reload = True
+#         app.run(HOST, PORT)
 
     
