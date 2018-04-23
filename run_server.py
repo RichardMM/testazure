9b05bd2279cd89993bdbf5b8ba928a615386d80e
RichardMM <rmachaz@gmail.com>
2018-04-23 08:30:21 +0300

    change app to manager.py

--- run_server.py
+++ run_server.py
@@ -14,25 +14,4 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', 'localhost'))
     except ValueError:
         PORT = 8080
-    app.run(HOST, PORT)
-# except (ModuleNotFoundError, ImportError, ImportWarning) as ex:
-#     # logging.basicConfig(filename="myapp.log", level=logging.WARNING)
-#     # logging.exception(ex)
-#     # log import error in new app
-#     from flask import Flask
-#     app = Flask(__name__)
-#     @app.route('/', methods=['GET'])
-#     def errohandler():
-#         var = traceback.format_exc()
-#         return var
-#     if __name__ == '__main__':
-#         # db.create_all()
-#         HOST = environ.get('SERVER_HOST', 'localhost')
-#         try:
-#             PORT = int(environ.get('SERVER_PORT', 'localhost'))
-#         except ValueError:
-#             PORT = 8080
-#         #app.jinja_env.auto_reload = True
-#         app.run(HOST, PORT)
-
-    
+    manager.run()
